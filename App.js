84cb4eb8cfa33793e9aa94a9c117f8bd93f69bd3
Julia Pagola <pagola.132511@e.unavarra.es>
2024-04-11 16:41:36 +0200

    Componentes React Native

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View } from "react-native";
+import Campobase from "./componentes/CampobaseComponent";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View>
+        <Campobase /><StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
